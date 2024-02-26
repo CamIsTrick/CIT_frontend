d80f298696e153f857bede9d55edecfc5c923cc1
YOON-CC <whdbscks77@gmail.com>
2024-02-26 19:22:05 +0900

    feat: Main페이지 반응형 및 컴포넌트 분리작업 완료 (#3)

---
+++ src/pages/Main/components/MainChatting.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const MainChatting = () => {
+    return (
+        <S.MainChattingLayout>
+            <S.MainChattingContainer/>
+        </S.MainChattingLayout>
+    );
+};
+
+const S = {
+    MainChattingLayout: styled.div`
+        background-color: purple;
+        color: #fff;
+        height: 75%;
+        width : 100%;
+    `,
+    MainChattingContainer: styled.div`
+        background-color: white;
+        color: #fff;
+        width: 90%;
+        height: 90%;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    `,
+};
+
+export default MainChatting;
---
+++ src/pages/Main/components/MainSetting.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const MainSetting = () => {
+    return (
+        <S.MainSettingLayout>
+            <S.MainSettingContainer>
+                
+            </S.MainSettingContainer>
+        </S.MainSettingLayout>
+    );
+};
+
+const S = {
+    MainSettingLayout: styled.div`
+        background-color: yellow    ;
+        color: #fff;
+        height: 25%;
+        width : 100%;
+    `,
+    MainSettingContainer: styled.div`
+        background-color: white;
+        color: #fff;
+        width: 90%;
+        height: 80%;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    `,
+};
+
+export default MainSetting;
