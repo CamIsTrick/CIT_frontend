78f85f40b55e34d1b5bf7953c2eb25b7b08671f3
YOON-CC <whdbscks77@gmail.com>
2024-03-01 15:52:13 +0900

    feat: webpack 설치 및 테스트 예제 적용 (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import * as React from "react";
 import Main from './pages/Main/Main';
+import myImage1 from './assets/num1.png'; 
+import myImage2 from './assets/num2.png';
+
 
 function App() {
+  
+  const [myImage3, setMyImage3] = React.useState(null);
+
+  const handleImageLoad = () => {
+    import('./assets/num3.png').then(imageModule => {
+      setMyImage3(imageModule.default);
+    });
+  };
+
   return (
     <>
       <Main/>
+      <div>TEST</div>
+      <img src={myImage1} alt="My Image" />
+      <img src={myImage2} alt="My Image" />
+      {myImage3 && <img src={myImage3} alt="My Image" />}
+      <button onClick={handleImageLoad}>Load Image 3</button>
     </>
   );
 }
--- src/pages/Main/components/MainHeader.tsx
+++ src/pages/Main/components/MainHeader.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import * as React from "react";
 import styled from 'styled-components';
 
 const MainHeader = () => {
 
     return (
         <S.MainHeader>
-            <S.MainHeaderLogo src="주소" alt="로고" />
+            {/* Failed to load resource: the server responded with a status of 404 (Not Found)
+            뜰건데 이거는 지금 주소 없어서 그런것 */}
+            {/* <S.MainHeaderLogo src="주소" alt="로고" /> */} 
         </S.MainHeader>
     );
 };
