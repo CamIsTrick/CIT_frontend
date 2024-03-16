f880f15b61c3225473f3a9e633c6307eff8cb99b
임성경 <131395142+5622lsk@users.noreply.github.com>
2024-03-17 02:18:04 +0900

    Update AnonButton.tsx
    컨벤션 지켜서 다시 수정했습니다!

--- src/pages/Main/components/AnonButton.tsx
+++ src/pages/Main/components/AnonButton.tsx
@@ -2,45 +2,10 @@ import * as React from "react";
 import { useState } from 'react';
 import styled from 'styled-components';
 
-
-const AnonButtonWrapper = styled.div`
-  display: inline-block;
-  cursor: pointer;
-`;
-
 interface SwitchProps {
   isOn: boolean;
 }
 
-const Switch = styled.div<SwitchProps>`
-  position: relative;
-  display: inline-block;
-  width: 55px;
-  height: 25px;
-  background-color: ${({ isOn }) => (isOn ? '#FF6B6B' : '#ccc')};
-  border-radius: 15px;
-  transition: background-color 0.3s ease;
-`;
-
-const SwitchCircle = styled.div<SwitchProps>`
-  position: absolute;
-  top: 50%;
-  transform: ${({ isOn }) => (isOn ? 'translate(calc(100% + 17px), -50%)' : 'translate(2px, -50%)')};
-  width: 18px;
-  height: 18px;
-  background-color: #fff;
-  border-radius: 50%;
-  transition: transform 0.3s ease;
-`;
-
-const Label = styled.span<SwitchProps>`
-  position: absolute;
-  top: 50%;
-  ${({ isOn }) => (isOn ? 'left: 17px;' : 'right: -7px;')};
-  transform: translate(-50%, -50%);
-  font-weight: bold;
-  font-size: 14px;
-`;
 
 const AnonButton: React.FC = () => {
   const [isAnonymous, setIsAnonymous] = useState<boolean>(false);
@@ -50,13 +15,48 @@ const AnonButton: React.FC = () => {
   };
 
   return (
-    <AnonButtonWrapper onClick={toggleAnonymous}>
-      <Switch isOn={isAnonymous}>
-        <SwitchCircle isOn={isAnonymous} />
-        <Label isOn={isAnonymous}>{isAnonymous ? '익명' : '실명'}</Label>
-      </Switch>
-    </AnonButtonWrapper>
+    <S.AnonButtonWrapper onClick={toggleAnonymous}>
+      <S.Switch isOn={isAnonymous}>
+        <S.SwitchCircle isOn={isAnonymous} />
+        <S.Label isOn={isAnonymous}>{isAnonymous ? '익명' : '실명'}</S.Label>
+      </S.Switch>
+    </S.AnonButtonWrapper>
   );
 };
+
+const S = {
+    AnonButtonWrapper: styled.div`
+      display: inline-block;
+      cursor: pointer;
+    `,
+    Switch: styled.div<SwitchProps>`
+      position: relative;
+      display: inline-block;
+      width: 55px;
+      height: 25px;
+      background-color: ${({ isOn }) => (isOn ? '#FF6B6B' : '#ccc')};
+      border-radius: 15px;
+      transition: background-color 0.3s ease;
+    `,
+    SwitchCircle: styled.div<SwitchProps>`
+      position: absolute;
+      top: 50%;
+      transform: ${({ isOn }) => (isOn ? 'translate(calc(100% + 17px), -50%)' : 'translate(2px, -50%)')};
+      width: 18px;
+      height: 18px;
+      background-color: #fff;
+      border-radius: 50%;
+      transition: transform 0.3s ease;
+    `,
+    Label: styled.span<SwitchProps>`
+      position: absolute;
+      top: 50%;
+      ${({ isOn }) => (isOn ? 'left: 17px;' : 'right: -7px;')};
+      transform: translate(-50%, -50%);
+      font-weight: bold;
+      font-size: 14px;
+    `,
+};
+
 
 export default AnonButton;
